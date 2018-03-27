bd07dd2ef30599bb5521f579d1ea8611782a890d
Seong Yong-ju <sei40kr@gmail.com>
2018-03-27 12:09:04 +0900

    Add union-find-with-ranks

---
+++ union-find/union-find-with-ranks.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+
+// union-find.cpp
+// author: Seong Yong-ju <sei40kr@gmail.com>
+
+// Union-Find 木の実装
+// cf https://www.slideshare.net/chokudai/union-find-49066733
+const int MAX_N = 2014;
+
+int par[MAX_N];
+int rank[MAX_N];
+
+void init(int n) {
+  REP(i, n) {
+    par[i] = i;
+    rank[i] = 0;
+  }
+}
+
+int root(int x) {
+  return par[x] == x ? x : par[x] = root(par[x]);
+}
+
+bool same(int x, int y) {
+  return root(x) == root(y);
+}
+
+void unite(int x, int y) {
+  x = root(x);
+  y = root(y);
+  if (x == y) {
+    return;
+  }
+
+  if (rank[x] < rank[y]) {
+    par[x] = y;
+    return;
+  }
+
+  if (rank[x] == rank[y]) {
+    rank[x]++;
+  }
+  rank[y] = x;
+}
