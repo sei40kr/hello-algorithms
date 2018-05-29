9429f85d0b61961a643a0634c1e588fd2a443400
Seong Yong-ju <sei40kr@gmail.com>
2018-05-30 00:13:45 +0900

    Add warshall-floyd

---
+++ warshall-floyd/warshall-floyd.rs
@@ -0,0 +1,13 @@
+// warshall-floyd.rs --- Warshall-Floyd
+// author: Seong Yong-ju <sei40kr@gmail.com>
+
+#[allow(dead_code)]
+fn warshall_floyd(edges: usize, dists: &mut [&mut [i32]]) {
+    for i in 0..edges {
+        for j in 0..edges {
+            for k in 0..edges {
+                dists[i][j] = dists[i][j].min(dists[i][k] + dists[k][j]);
+            }
+        }
+    }
+}
