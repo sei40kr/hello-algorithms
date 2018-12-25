67554eefe3e0881c91f69612ee540643609b9988
Seong Yong-ju <sei40kr@gmail.com>
2018-12-25 21:54:34 +0900

    Fix warshall-floyd

--- warshall-floyd/warshall-floyd.rs
+++ warshall-floyd/warshall-floyd.rs
@@ -3,9 +3,9 @@
 
 #[allow(dead_code)]
 fn warshall_floyd(edges: usize, dists: &mut [&mut [i32]]) {
-    for i in 0..edges {
-        for j in 0..edges {
-            for k in 0..edges {
+    for k in 0..edges {
+        for i in 0..edges {
+            for j in 0..edges {
                 dists[i][j] = dists[i][j].min(dists[i][k] + dists[k][j]);
             }
         }
