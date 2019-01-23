ad19be8a13615b582e6314ace55a8dcdd9adfa84
Seong Yong-ju <sei40kr@gmail.com>
2019-01-23 23:01:44 +0900

    Clean up djikstra

--- djikstora/djikstora.rs
+++ djikstora/djikstora.rs
@@ -2,21 +2,6 @@ use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::u32;
 
-impl Ord for State {
-    fn cmp(&self, other: &State) -> Ordering {
-        other
-            .cost
-            .cmp(&self.cost)
-            .then_with(|| self.node.cmp(&other.node))
-    }
-}
-
-impl PartialOrd for State {
-    fn partial_cmp(&self, other: &State) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
 struct Edge {
     node: usize,
     cost: u32,
@@ -28,23 +13,31 @@ struct State {
     cost: u32,
 }
 
+impl Ord for State {
+    fn cmp(&self, other: &State) -> Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &State) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 #[allow(dead_code)]
-fn shortest_path(n: usize, s: usize, g: usize, edges_by_from: &Vec<Vec<Edge>>) -> Option<u32> {
+fn shortest_path(n: usize, s: usize, g: usize, edges: &Vec<Vec<Edge>>) -> Option<u32> {
     let mut dists: Vec<_> = vec![u32::MAX; n];
     dists[s] = 0;
 
     let mut queue = BinaryHeap::new();
-    queue.push(State { cost: 0, node: s });
+    queue.push(State { node: s, cost: 0 });
 
     while let Some(State { node, cost }) = queue.pop() {
-        if node == g {
-            return Some(cost);
-        }
-
         if cost < dists[node] {
             dists[node] = cost;
 
-            for edge in &edges_by_from[node] {
+            for edge in &edges[node] {
                 queue.push(State {
                     node: edge.node,
                     cost: cost + edge.cost,
@@ -53,5 +46,9 @@ fn shortest_path(n: usize, s: usize, g: usize, edges_by_from: &Vec<Vec<Edge>>) -
         }
     }
 
-    None
+    if dists[g] != u32::MAX {
+        Some(dists[g])
+    } else {
+        None
+    }
 }
