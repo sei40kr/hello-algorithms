33ed758c88a2ebc21791d81e1e577651e9297ae6
Seong Yong-ju <sei40kr@gmail.com>
2019-01-28 00:35:08 +0900

    Add Rust implementation for lis

---
+++ longest-increasing-subsequence/lis-fast.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::i32;
+
+// lis-fast.rs --- Longest Increasing Subsequence
+// author: Seong Yong-ju <sei40kr@gmail.com>
+
+#[allow(dead_code)]
+fn lis(vec: &Vec<i32>) -> usize {
+    let mut dp = vec![i32::MAX; vec.len()];
+
+    for &x in vec.into_iter() {
+        // x extends largest subsequence, or will become end candidate of an
+        // existing subsequence.
+        let i = dp.binary_search(&x).unwrap();
+        dp[i] = x;
+    }
+
+    return match dp.binary_search(&i32::MAX) {
+        Ok(i) => i,
+        Err(i) => i,
+    };
+}
